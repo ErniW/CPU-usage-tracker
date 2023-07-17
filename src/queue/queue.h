4bac7dd7869fc33d5cfb84c204e75eac61983582
Ernest Warzocha <ernestwarzocha@gmail.com>
2023-07-17 12:47:25 +0200

    function descriptions

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -17,6 +17,23 @@ struct Queue{
 
 typedef struct Queue Queue;
 
+/**
+ * Initialize the queue and allocate its buffer. You can change buffer size 
+ * in BUFFER_SIZE macro in queue.h
+ * @param queue Queue object to initialize.
+ */
 void Queue_init(Queue* queue);
+
+/**
+ * Push data to buffer. 
+ * @param queue Queue object to push.
+ * @param data current state as raw data. Queue_push converts the value
+ * and then places it into a buffer.
+ */
 void Queue_push(Queue* queue, FILE* data);
+
+/**
+ * Pop data from buffer.
+ * @param queue Queue object to get value from.
+ */
 CPU_state Queue_pop(Queue* queue);
